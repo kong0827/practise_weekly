fb1f7689a0b1332f81cd8f103bb2a8d54b8c6173
kongxiangjin <1351882069@qq.com>
2019-11-14 15:25:47 +0800

    lru

---
+++ LRUCache/src/main/java/com/kxj/algorithms/LRUCache.java
@@ -0,0 +1,37 @@
+package com.kxj.algorithms;
+
+import sun.plugin2.message.GetAppletMessage;
+
+/**
+ * @ClassName LRUCache
+ * @Description TODO
+ * @Author kongxiangjin
+ * @Date 2019/11/14 14:47
+ * @Version 1.0
+ **/
+public class LRUCache {
+    public LRUCache(int capacity) {
+
+    }
+
+    public String get(int key) {
+        return null;
+    }
+
+    public void put(int key, int value) {
+
+    }
+
+    public boolean del(int key) {
+        return true;
+    }
+
+    public int length(LRUCache lruCache) {
+        return 1;
+    }
+
+    public void reset(LRUCache lruCache) {
+
+    }
+
+}
---
+++ LRUCache/src/test/java/com/kxj/LRUCacheTest.java
@@ -0,0 +1,100 @@
+package com.kxj;
+
+import com.kxj.algorithms.LRUCache;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @ClassName LRUCacheTest
+ * @Description TODO
+ * @Author kongxiangjin
+ * @Date 2019/11/14 14:52
+ * @Version 1.0
+ **/
+public class LRUCacheTest {
+    @Test
+    public void test1() {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 'a');
+        lruCache.put(2, 'b');
+        String result = lruCache.get(1);
+        Assert.assertEquals('a', result);
+        result = lruCache.get(2);
+        Assert.assertEquals('b', result);
+        result = lruCache.get(3);
+        Assert.assertEquals(null, result);
+    }
+
+    @Test
+    public void test2() {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 'a');
+        lruCache.put(2, 'b');
+        lruCache.put(3, 'c');
+
+        String result = lruCache.get(3);
+        Assert.assertEquals('c', result);
+
+        result = lruCache.get(1);
+        Assert.assertEquals(null, result);
+    }
+
+    @Test
+    public void test3() {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 'a');
+        lruCache.put(2, 'b');
+
+        String result = lruCache.get(1);
+        Assert.assertEquals('a', result);
+
+        lruCache.del(1);
+        result = lruCache.get(1);
+        Assert.assertEquals(null, result);
+
+        int lruCacheLength = lruCache.length(lruCache);
+        Assert.assertEquals(1, lruCacheLength);
+
+        lruCache.put(3, 'c');
+        result = lruCache.get(3);
+        Assert.assertEquals('c', result);
+        lruCacheLength = lruCache.length(lruCache);
+        Assert.assertEquals(2, lruCacheLength);
+    }
+
+    @Test
+    public void test4() {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 'a');
+        lruCache.put(2, 'b');
+
+        String result = lruCache.get(2);
+        Assert.assertEquals('b', result);
+
+        lruCache.reset(lruCache);
+
+        result = lruCache.get(1);
+        Assert.assertEquals(null, result);
+        int lruCacheLength = lruCache.length(lruCache);
+        Assert.assertEquals(0, lruCacheLength);
+
+        lruCache.put(2, 'b');
+        result = lruCache.get(2);
+        Assert.assertEquals('b', result);
+        lruCacheLength = lruCache.length(lruCache);
+        Assert.assertEquals(1, lruCacheLength);
+
+    }
+
+    @Test
+    public void test5() {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 'a');
+        lruCache.put(2, 'b');
+        lruCache.put(2, 'd');
+
+        String result = lruCache.get(2);
+        Assert.assertEquals('d', result);
+    }
+
+}
