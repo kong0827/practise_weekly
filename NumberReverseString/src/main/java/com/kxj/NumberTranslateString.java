b5c4ea43e1533538e9e4c4e5d77b600aa6f6c5d1
kongxiangjin <1351882069@qq.com>
2020-06-15 18:25:19 +0800

    数字转字符串

--- NumberReverseString/src/main/java/com/kxj/NumberTranslateString.java
+++ NumberReverseString/src/main/java/com/kxj/NumberTranslateString.java
@@ -6,18 +6,10 @@ package com.kxj;
  * @desc 面试题46. 把数字翻译成字符串
  * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
  * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
- * <p>
- * <p>
- * <p>
- * 示例 1:
- * <p>
  * 输入: 12258
  * 输出: 5
  * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
- * <p>
- * <p>
  * 提示：
- * <p>
  * 0 <= num < 231
  */
 public class NumberTranslateString {
@@ -27,20 +19,14 @@ public class NumberTranslateString {
     }
 
     public static int translateNum(int num) {
-        int count = 1;
-
-        while (num > 0 && num >= 10) {
-            count++;
-            int reverseNumber = 0;
-            reverseNumber = reverseNumber * 10 + num % 10;
-            num = num / 10;
-            reverseNumber = num % 10 * 10 + reverseNumber;
-
-            if (reverseNumber / 25 > 1) {
-                count--;
-            }
-
+        String s = String.valueOf(num);
+        int a = 1, b = 1;
+        for(int i = 2; i <= s.length(); i++) {
+            String tmp = s.substring(i - 2, i);
+            int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+            b = a;
+            a = c;
         }
-        return count;
+        return a;
     }
 }
