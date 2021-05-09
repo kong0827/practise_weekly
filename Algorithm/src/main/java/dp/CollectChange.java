5ff3c4c20791b821271855ce226a3589c9cde1e7
kong0827 <1351882069@qq.com>
2021-05-10 00:40:59 +0800

    凑零錢

--- Algorithm/src/main/java/dp/CollectChange.java
+++ Algorithm/src/main/java/dp/CollectChange.java
@@ -1,9 +1,45 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * @author kxj
  * @date 2021/5/9 18:17
  * @desc
  */
 public class CollectChange {
+
+    public static void main(String[] args) {
+        int k = 3;
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+
+        int helper = helper(k, amount, coins);
+        System.out.println(helper);
+    }
+
+    private static int helper(int k, int amount, int[] coins) {
+        if (amount < 0) {
+            return -1;
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (Arrays.asList(coins).contains(amount)) {
+            return 1;
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+
+            int subProblem = helper(k, amount - coins[i], coins);
+
+            if (subProblem == -1) {
+                continue;
+            }
+            res = Math.min(res, 1 + subProblem);
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+
+    }
+
 }
