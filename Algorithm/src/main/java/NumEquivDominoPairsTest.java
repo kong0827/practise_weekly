e7dd261f29282f2a83466dd4fc3def908dde8dd8
kongxiangjin <1351882069@qq.com>
2021-05-31 18:59:37 +0800

    多米诺骨牌

--- Algorithm/src/main/java/NumEquivDominoPairsTest.java
+++ Algorithm/src/main/java/NumEquivDominoPairsTest.java
@@ -69,6 +69,16 @@ public class NumEquivDominoPairsTest {
         return ans;
     }
 
+    public static int numEquivDominoPairs3(int[][] dominoes) {
+        int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
+            int val = domino[0] < domino[1] ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
 }
 
 /**
